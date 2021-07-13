7e50a078f4f0d301d12d7745080f3f9fbe4b7d44
LotusDeath69 <LotusDeath69@gmail.com>
2021-07-12 23:27:49 -0400

    second commit

---
+++ database.py
@@ -0,0 +1,38 @@
+import sqlite3
+con = sqlite3.connect('test.db')
+cur = con.cursor()
+
+
+def createConnection():
+    global con
+    global cur
+    con = sqlite3.connect('test.db')
+    cur = con.cursor()
+    
+    
+def closeConnection():
+    con.close()
+    
+    
+def logRetrive():
+    createConnection()
+    with con:
+        cur.execute('SELECT * FROM logs WHERE name=:name ', {"name": 'Tree'})
+    return cur.fetchall()
+
+
+def logAdd(date, hours, total_hours):
+    createConnection()
+    with con:
+        cur.execute('INSERT INTO logs VALUES (:date, :hours, :name, :totalHours)', {"date": date, "hours": hours, "name": 'Tree', "totalHours": total_hours})
+
+
+
+
+# cur.execute('''CREATE TABLE logs
+#                (date text, hours text, name text, totalHours text)''')
+# logAdd('test1', 'test2', 'test3')
+# print(logRetrive())
+
+con.commit()
+closeConnection()
